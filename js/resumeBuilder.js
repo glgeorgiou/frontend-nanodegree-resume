2e8e86c3987b4d6ee6c489ec3426b7b68e9eeb75
GGEORGIOU <ggeorgiou.gr@gmail.com>
2015-09-28 12:13:33 +0300

    add Name and Role into resubeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/**
+* Append Name and role into page header using HTMLheaderName & HTMLheaderRole variables from helper.js
+* and replace the %data% value with attributes and append them into index.html
+*/
+var myName = "Georgios Georgiou"; 
+var formattedName = HTMLheaderName.replace("%data%",myName);
+var myRrole = "Web Implementer";
+var formattedRole = HTMLheaderRole.replace("%data%",myRrole);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
