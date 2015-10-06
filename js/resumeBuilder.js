4d72534b17f920849a89e5c66bfad024757d7d23
GGEORGIOU <ggeorgiou.gr@gmail.com>
2015-10-06 19:13:46 +0300

    my resume after level 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -73,7 +73,8 @@ var education = {
   		"dates" : "2014",
   		"location" : "Patra, Greece",
       "description" : "This is a description of the job in to employer 2"
-  	}]
+  	}
+    ]
   }
 
   //Projects Object
@@ -81,25 +82,37 @@ var education = {
   var projects = {
   	"projects" : [
   	{
-  		"title" : "Sample project 1",
+  		"title" : "APM",
   		"dates" : 2014,
-  		"description" : "bla bla bla",
+  		"description" : "A WordPress site for Mathematics and Physics",
   		"images" : 
         [
-  			"http://www.project1",
-  			"http://www.project2"
+  			"http://www.ggeorgiou.gr/wp-content/uploads/2015/04/website-apm-new-design-895x460.jpg",
+        "http://www.ggeorgiou.gr/wp-content/uploads/2015/04/website-apm-new-design-150x150.jpg"
   		  ]
-  	 }
+  	 },
+      {
+      "title" : "Hair Gallery",
+      "dates" : 2010,
+      "description" : "A WordPress site for a hair dresser",
+      "images" : 
+        [
+        "http://www.ggeorgiou.gr/wp-content/uploads/2015/04/website-hair-gallery1-895x460.jpg",
+        "http://www.ggeorgiou.gr/wp-content/uploads/2015/04/website-hair-gallery1-150x150.jpg"
+        ]
+     }
   	]
   }
 
+
 /***
-* Add Name and role
+* Add Name and role. 
 */
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
+
 
 
   /**
@@ -119,11 +132,12 @@ if (bio.skills.length > 0)
    $("#skills").append(formattedSkill);
 }
 
+
 /**
 * Jobs using for-in loop
 */
 
-for (job in work.jobs) 
+/*for (job in work.jobs) 
 {
   $("#workExperience").append(HTMLworkStart); //Append the work element
 
@@ -138,4 +152,77 @@ for (job in work.jobs)
   $(".work-entry:last").append(formattedDates);
   $(".work-entry:last").append(formattedLocation);
   $(".work-entry:last").append(formattedDescription);
+}*/
+/**
+* Jobs using for-in loop inside the function
+*/
+function displayWork ()
+{
+for (job in work.jobs) 
+  {
+    $("#workExperience").append(HTMLworkStart); //Append the work element
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer+formattedTitle; //Concatenation
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+    $(".work-entry:last").append(formattedEmployerTitle); //append to the last element of the class
+    $(".work-entry:last").append(formattedDates);
+    $(".work-entry:last").append(formattedLocation);
+    $(".work-entry:last").append(formattedDescription);
+  }
 }
+displayWork ();
+
+
+/**
+* Adds a Internationalization button and Capitalize the sirname using function
+
+function inName(name) {
+  name = name.split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = names[0].slice(0,1).toUpperCase()+name[0].slice(1).toLowerCase();
+
+  return name[0]+" "+name[1];
+}
+inName("george georgiou");
+$("#main").append(internationalizeButton);*/
+
+
+/**
+* Display projects
+*/
+
+projects.display = function () {
+  for (project in projects.projects) {
+    $("#projects").append(HTMLprojectStart); //Append the project element
+
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedTitle);
+
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedDescription);
+
+    if (projects.projects[project].images.length > 0) {
+      for (image in projects.projects[project].images) {
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        $(".project-entry:last").append(formattedImage);
+      }
+    }
+  }
+}
+
+projects.display();
+
+
+/**
+* Display the map
+*/
+$("#mapDiv").append(googleMap);
